9c070e404d77ebdcb24542e2ad9d4ce5aad2b3a0
Yi Wu <ywu676@wisc.edu>
2024-12-03 18:27:43 -0600

    fix issue #204 w/ help of @stevealkr

--- sc2/unit.py
+++ sc2/unit.py
@@ -1026,7 +1026,7 @@ class Unit:
         """ Returns the a list of the current orders. """
         # TODO: add examples on how to use unit orders
         return [UnitOrder.from_proto(order, self._bot_object) for order in self._proto.orders
-            if order.ability_id in self._bot_object._game_data.abilities]
+            if order.ability_id in self._bot_object.game_data.abilities]
 
     @cached_property
     def order_target(self) -> Optional[Union[int, Point2]]:
